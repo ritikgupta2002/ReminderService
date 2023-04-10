f89c0bc8701b3d993634f650ab9b516e1430ee67
ritik <gupta.ritik2002@gmail.com>
2023-04-10 11:21:44 +0530

    Added notification Ticket model

--- src/index.js
+++ src/index.js
@@ -3,18 +3,24 @@ const bodyParser = require("body-parser");
 
 const { PORT } = require("./config/serverConfig");
 
-const { sendBasicEmail } = require("./services/email-service");
+// const { sendBasicEmail } = require("./services/email-service");
+
+const cron = require("node-cron");
+
 const setupAndStartServer = () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.listen(PORT, () => {
     console.log(`Server started at PORT:${PORT}`);
-    sendBasicEmail(
-      "notificationservice2002@gmail.com",
-      "This is a testing email",
-      "Hey how are you , I hope you like the support 😍"
-    );
+    // sendBasicEmail(
+    //   "notificationservice2002@gmail.com",
+    //   "This is a testing email",
+    //   "Hey how are you , I hope you like the support 😍"
+    // );
+    cron.schedule('* * * * *', () => {
+      console.log('running a task every minute');
+    });
   });
 };
 
